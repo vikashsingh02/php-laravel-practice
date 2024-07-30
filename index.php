830f9d9a0821951a926a2c11c4bf87dc3a59bd17
vikashsingh02 <107889207+vikashsingh02@users.noreply.github.com>
2024-07-30 20:20:57 +0530

    adding index

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php   
+
+// for($a=1;$a<=10;$a++){
+//    for($b=$a;$b<=5;$b++){
+//       echo"$a";
+//    }
+//    echo"</br>";
+// }
+/* string 
+strlen -- string length 
+str_replace -- to replace strig charcters 
+str_ireplace -- not an case sensitive 
+ucwords -- it captilize first letter of every sentence present in string
+ucfirst -- it caotilize only first letter of string
+ 
+*/
+$name = "vikash singh doon";
+echo "Name string has  ". strlen($name) . "  characters ";
+echo "<br>";
+echo str_Ireplace("V","X" ,$name);
+echo "<br>";
+echo ucwords($name);
+echo "<br>";
+echo ucfirst($name);
+echo "<br>";
+echo strtoupper($name);
+echo "<br>";
+echo strtolower($name);
+?>
